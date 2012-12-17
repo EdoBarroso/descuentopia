ea60d7ac25ddc893f85697aca8bf781abb13a97a
Eduardo Barroso <edo@barro.so>
2012-12-17 13:56:19 -0300

    Add gravatar profile image and root to user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Tudescuento::Application.routes.draw do
   authenticated :user do
-    root :to => 'home#index'
+    root :to => 'users#profile'
   end
   root :to => "home#index"
   devise_for :users
