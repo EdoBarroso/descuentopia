c310f0d69c5725cf560acbd21a400b6f01689961
Eduardo Barroso <edo@barro.so>
2012-12-26 00:54:40 -0300

    Automatically redirects to where user was on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
-  
+
   def after_sign_in_path_for(resource)
-    current_user
+    (session[:user_return_to].nil?) ? "/" : session[:user_return_to].to_s
   end
 
 end
