e090e253de51c3d85f45a989fb22e8d86de5ae11
Eduardo Barroso <edo@barro.so>
2013-04-15 15:42:30 -0300

    Venues page now contains Discount_Clubs

--- app/admin/venues.rb
+++ app/admin/venues.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register Venue do
-
+	form do |f|
+    f.inputs "Venue Details" do
+      f.input :name
+      f.input :category
+      f.input :discount_clubs
+    end
+    f.actions
+  end
 end
